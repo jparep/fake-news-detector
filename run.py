a489278494a3e22bcc5afcefcac7d641aae631b3
jparep <parepson@gmail.com>
2024-12-08 05:30:40 -0700

    Remove dynamic port and assigned static port

--- run.py
+++ run.py
@@ -4,7 +4,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    # Fetch the port from environment variables, default to 5000 if not provided
-    port = int(os.getenv("PORT", 5000))
-    debug = app.config["DEBUG"]
-    app.run(host="0.0.0.0", port=port, debug=debug)
+    app.run(debug=False)
